a1f8581cb82821eaf19cd6770590aa4c837b5c2d
Filipe <filipe.sferreira17@gmail.com>
2022-09-20 13:19:26 -0300

    Portfólio em Desenvolvimento

--- assets/js/menuAnimation.js
+++ assets/js/menuAnimation.js
@@ -1,22 +1,26 @@
-const menuIcon = document.querySelectorAll('#menu_icon');
-let menuList = document.querySelector('.header__menu');
+let menuIcon = document.querySelector('#menu_icon');
+const menuList = document.querySelector('.header__menu');
 
-menuIcon[0].addEventListener('click', () => {
-
-
+menuIcon.addEventListener('click', () => {
     if (menuList.classList.contains('hideMenu')) {
+        menuIcon.src = '../assets/img/x.png';
         menuList.classList.add('showMenu');
         menuList.classList.remove('hideMenu');
     } else {
+        menuIcon.src = '../assets/img/menu.png';
         menuList.classList.remove('showMenu');
         menuList.classList.add('hideMenu');
     }
 });
 
-const headerLinks = document.querySelector('.header__links', '.header__links');
+const menuLinks = document.querySelectorAll('#header__menu .header__links');
 
-headerLinks.addEventListener('click', ()=> {
-        if (menuList.classList.contains('showMenu')) {
+for(i = 0; i < menuLinks.length; i++) {
+    menuLinks[i].addEventListener('click', ()=> {
+        if(menuList.classList.contains('showMenu')) {
+            menuIcon.src = '../assets/img/menu.png';
             menuList.classList.add('hideMenu');
-    }
-});
+            menuList.classList.remove('showMenu');
+        }
+    });
+}
