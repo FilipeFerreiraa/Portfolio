06f595a9d82ea567a1521d7044cc6ed0fb1d72f9
Filipe <filipe.sferreira17@gmail.com>
2022-09-20 14:31:31 -0300

    v0.0.6

---
+++ assets/js/titleAnimation.js
@@ -0,0 +1,11 @@
+const spanMessage = ['Filipe Ferreira'];
+let textPosition = 0;
+
+typeWriter = () => {
+    document.querySelector('.home__span').innerHTML = spanMessage[0].substring(0, textPosition);
+    if(textPosition++ != spanMessage[0].length) {
+        setTimeout(typeWriter, 150);
+    }
+}
+
+window.addEventListener('load', typeWriter);
